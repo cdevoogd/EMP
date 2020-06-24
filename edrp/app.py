79af29e426cc2451a7ebffc088331b82d179b79b
Camden DeVoogd <camdendevoogd@gmail.com>
2020-06-24 14:05:04 -0700

    Cleaning up

--- edrp/app.py
+++ edrp/app.py
@@ -10,7 +10,6 @@ import plotly.express as px
 
 load_dotenv()
 
-# Load dataset
 earthquakes = pd.read_csv('edrp/data/clean_dataset.csv', header=0)
 px.set_mapbox_access_token(os.getenv('MAPBOX_ACCESS_TOKEN'))
 
@@ -36,29 +35,27 @@ app = dash.Dash(__name__)
 app.layout = html.Div(children=[
     html.H1(children='Earthquake Damage Risk Predictor'),
 
-    html.Form(
-        children=[
-            dcc.Input(
-                id='input_latitude',
-                persistence_type='session',
-                placeholder=default_latitude,
-                type='number'
-            ),
-            dcc.Input(
-                id='input_longitude',
-                persistence_type='session',
-                placeholder=default_longitude,
-                type='number'
-            ),
-            html.Button(
-                children='Submit',
-                className='button',
-                id='submit_location',
-                # Setting the button's type to 'button' prevents the page from being reloaded when it is pressed
-                type='button'
-            )
-        ]
-    ),
+    html.Form(children=[
+        dcc.Input(
+            id='input_latitude',
+            persistence_type='session',
+            placeholder=default_latitude,
+            type='number'
+        ),
+        dcc.Input(
+            id='input_longitude',
+            persistence_type='session',
+            placeholder=default_longitude,
+            type='number'
+        ),
+        html.Button(
+            children='Submit',
+            className='button',
+            id='submit_location',
+            # Setting the button's type to 'button' prevents the page from being reloaded when it is pressed
+            type='button'
+        )
+    ]),
 
     dcc.Graph(
         id='local_scattermap',
@@ -66,9 +63,8 @@ app.layout = html.Div(children=[
     )
 ])
 
-@app.callback(
-    Output('local_scattermap', 'figure'), [Input('submit_location', 'n_clicks')], [State('input_latitude', 'value'), State('input_longitude', 'value')])
-def update_scattermap_center(n_clicks, latitude, longitude):
+@app.callback(Output('local_scattermap', 'figure'), [Input('submit_location', 'n_clicks')], [State('input_latitude', 'value'), State('input_longitude', 'value')])
+def update_map_target(n_clicks, latitude, longitude):
     # This callback will fire when the page loads. We don't want to update the map until the button is actually clicked
     if n_clicks is not None:
         scattermap.update_layout(mapbox_center={ 'lat': latitude, 'lon': longitude }, mapbox_zoom=6)
