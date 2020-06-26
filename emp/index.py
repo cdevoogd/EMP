4234906f8c410dbdc35fd5671db8bbf1809b9a76
Camden DeVoogd <camdendevoogd@gmail.com>
2020-06-26 12:24:49 -0700

    Import server into index for WSGI apps

--- emp/index.py
+++ emp/index.py
@@ -17,7 +17,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from emp.app import app
+from emp.app import app, server
 from emp.layouts import dashboard
 
 app.layout = html.Div([
