2b79632e52e97a428cad9c363be907fe710a33c1
Camden DeVoogd <camdendevoogd@gmail.com>
2020-06-26 10:27:56 -0700

    Update site title

--- emp/app.py
+++ emp/app.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=['https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/flatly/bootstrap.min.css'])
+app.title = 'Earthquake Magnitude Predictor'
 server = app.server
